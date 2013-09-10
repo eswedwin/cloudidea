317caab635a681aed13a4c3310b4f44576c1f77a
eswedwin <eswedwin@gmail.com>
2013-09-11 00:28:28 +0200

    changed constants passed to homepage to lower

--- routes.js
+++ routes.js
@@ -41,7 +41,7 @@ var build_errfn = function(errmsg, response) {
 var indexfn = function(request, response) {
     response.render("homepage", {
 		name: Constants.APP_NAME,
-		title: Constants.TITLE,
+		page_title: Constants.TITLE,
 		product_name: Constants.PRODUCT_NAME,
 		twitter_username: Constants.TWITTER_USERNAME,
 		twitter_tweet: Constants.TWITTER_TWEET,
